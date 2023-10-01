18d3f374cf90f183021d922092f5ab7e2a7d0392
SenchaBka <aburiak@my.centennialcollege.ca>
2023-10-01 16:15:45 -0400

    Add comments to all files

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+/* server.js - Arsenii Buriak - 301266501 - 01/10/23 */
+
 const express = require('express')
 const app = express()
 
@@ -10,4 +12,4 @@ const indexRouter = require('./routes/index')
 app.use('/', indexRouter)
 
 app.listen(3000)
-console.log('Server running at http://localhost:3000/');
+console.log('Server running at http://localhost:3000/')
